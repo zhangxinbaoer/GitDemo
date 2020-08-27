3628aa628c18dffe1af67c5ffe4b271f1f2e8978
zhangao <zhangao@163.com>
2020-08-28 00:04:08 +0800

    合并分支Branch5

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -2,9 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
-        System.out.println("创建分支");
-        System.out.println("合并分支");
         System.out.println("合并Branch2");
     }
 }
