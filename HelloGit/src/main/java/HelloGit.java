64a5c066a9723486712f595b6f0080dcafd933f2
zhangao <zhangao@163.com>
2020-08-28 00:05:00 +0800

    合并分支Branch7

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -3,8 +3,6 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
-        System.out.println("创建分支");
-        System.out.println("合并分支");
-        System.out.println("合并Branch2");
+
     }
 }
